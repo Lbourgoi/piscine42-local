5c49195c31ae681c0e151cb59d4090c99bc96a7d
Lbourgoi <lbourgoi@student.42lausanne.ch>
2021-07-19 14:26:47 +0200

    continuer a coder

---
+++ piscine/C02/ex00/ft_strcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbourgoi <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 10:37:21 by lbourgoi          #+#    #+#             */
+/*   Updated: 2021/07/19 14:26:15 by lbourgoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	index;
+	index = 0;
+	while (src[index] != '\0');
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
